9d7341d695d176599239ead9e5a7c6bd5381f2b4
Jithesh kumar T <jitheshkumar.2401059@srec.ac.in>
2026-09-25 17:59:10 +0530

    Time: 2 ms (46.08%), Space: 94.4 MB (51.6%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minprice=prices[0];
-        int maxprofit=0;
-        
+        int minprice =prices[0];
+        int maxprofit =0;
         for(int i=1;i<prices.length;i++){
             if(minprice>prices[i]){
-                minprice=prices[i];
+                minprice =prices[i];
             }
-            int profit =prices[i]-minprice;
-            if(profit>maxprofit){
-                maxprofit=profit;
+            int profit = prices[i] -minprice ;
+            if(profit > maxprofit){
+                maxprofit = profit ;
             }
-
         }
-       return maxprofit;
+        return maxprofit ;
+        
     }
 }
